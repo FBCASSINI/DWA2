78ee3978903780de39e36c1058968965b1990107
Flavio Cassini <fbcassini@fullsail.edu>
2017-01-05 17:48:11 -0500

    not working

---
+++ gravity/src/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function(req, res){
+    res.json({"items": [{healthy: true}]})
+});
+
+app.listen(port, function(){
+    console.log('Our server is running on port', port);
+});
